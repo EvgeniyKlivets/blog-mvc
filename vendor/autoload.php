7c4a72c60fac8856b3b2f0d650ddc3e2dfda720f
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-16 22:21:14 +0300

    Add files via upload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,6 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
+return ComposerAutoloaderInit16a922378a11a841d0e8c8e242389d7b::getLoader();
 return ComposerAutoloaderInit70ad2c628499252ae72eb1b7b8339510::getLoader();
+
