9905e71f4a29e3913960d00b2601b426d880a4ab
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-17 16:57:15 +0300

    Add files via upload

--- Public/index.php
+++ Public/index.php
@@ -5,9 +5,18 @@ require_once dirname(__DIR__ ). '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 
-use Config\Config;
+/*use Core\Model;
 
-Config::get('db.host');
+dd(Model::connect()); при public*/
+
+/*$query = Model:: connect()->query("SELECT*FROM users");
+$query->execute(); /*IT'S where SQL Database has*/
+//dd($query->fetchAll());
+
+
+
+/*use Config\Config;*/
+/*Config::get('db.test');*/
 /*dd(Config::get('db.test')); - лож*/
 /*dd (Config::get('db.host.test'), Config::get('db.user'));*/
 
