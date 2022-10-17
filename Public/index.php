48c78398b409240ff9dd85122912374fd1817ac2
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-17 19:31:42 +0300

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -5,19 +5,3 @@ require_once dirname(__DIR__ ). '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 
-/*use Core\Model;
-
-dd(Model::connect()); при public*/
-
-/*$query = Model:: connect()->query("SELECT*FROM users");
-$query->execute(); /*IT'S where SQL Database has*/
-//dd($query->fetchAll());
-
-
-
-/*use Config\Config;*/
-/*Config::get('db.test');*/
-/*dd(Config::get('db.test')); - лож*/
-/*dd (Config::get('db.host.test'), Config::get('db.user'));*/
-
-/*dd (__File__);*/
