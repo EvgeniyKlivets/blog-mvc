52100c88f95dca5ed9c35bfc9468fe2be40e63b0
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-23 21:01:13 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-//$router->add ('posts', ['controller' => 'PostsController', 'action' => 'index', 'method' => 'GET']);
+$router->add ('posts', ['controller' => 'PostsController', 'action' => 'index', 'method' => 'GET']);
 $router->add (
-    'posts/{id:\d+}',
+    'blog/{id:\D+}',
     ['controller' => \App\Controllers\PostController::class, 'action' => 'show', 'method' => 'GET']);
 
-//dd(__FILE__);
+
