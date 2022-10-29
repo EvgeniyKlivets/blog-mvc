947e0afc1e166f0c0218df6d5b0d39cc20a7a399
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-29 21:53:23 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-$router->add ('posts', ['controller' => 'PostsController', 'action' => 'index', 'method' => 'GET']);
-$router->add (
-    'blog/{id:\D+}',
-    ['controller' => \App\Controllers\PostController::class, 'action' => 'show', 'method' => 'GET']);
+//$router->add ('posts', ['controller' => 'PostsController', 'action' => 'index', 'method' => 'GET']);
+//$router->add ('blog/{id:\D+}',['controller' => \App\Controllers\PostController::class, 'action' => 'show', 'method' => 'GET']);
 
-
+$router->add ('login', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'login', 'method'=>'GET']);
+$router->add ('registration', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'register', 'method'=>'GET']);
+$router->add ('verify', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'verify', 'method'=>'POST']);
+$router->add ('users/store', ['controller'=>\App\Controllers\UsersController::class, 'action'=>'register', 'method'=>'POST']);
