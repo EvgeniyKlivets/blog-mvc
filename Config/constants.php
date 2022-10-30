0a7868b2d235fd3ae36533c28e0430e4e8f1b8a4
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-30 22:35:35 +0200

    Update constants.php

--- Config/constants.php
+++ Config/constants.php
@@ -1,4 +1,6 @@
 <?php
+define('SERVER_PORT', (!empty($_SERVER ['SERVER_PORT']) ? ':' . $_SERVER ['SERVER_PORT'] : ''));
+define('SITE_URL', $_SERVER ['REQUEST_SCHEME'] . '://' . $_SERVER ['SERVER_NAME'] . SERVER_PORT);
 
 define('BASE_DIR', dirname(__DIR__));
 define('APP_DIR', dirname(__DIR__). '/App');
