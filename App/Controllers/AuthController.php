b9afc30c02dd4ce1496b178a88647d4ae76d7410
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-30 21:33:23 +0200

    Update AuthController.php

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controllers;
 
+use Core\Controller;
 
-
-class AuthController 
+class AuthController extends Controller
 {
     public function login()
     {
-
+        dd(__METHOD__);
     }
 
     public function register ()
@@ -23,6 +23,15 @@ class AuthController
 
     public function store()
     {
-        
+
     }
-} 
+
+    /*public function before(string $action):bool
+    {
+        if ($action === 'login'){
+            header('Location: localhost:8888');
+            exit;
+        }
+        return false;
+    }*/
+}
