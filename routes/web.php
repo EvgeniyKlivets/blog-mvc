a5a401c667d572f7e5536a17611cae9c7d2e42bd
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-10-30 23:26:39 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@
 
 $router->add ('login', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'login', 'method'=>'GET']);
 $router->add ('registration', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'register', 'method'=>'GET']);
-$router->add ('verify', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'verify', 'method'=>'POST']);
+$router->add ('auth/verify', ['controller'=>\App\Controllers\AuthController::class, 'action'=>'verify', 'method'=>'POST']);
 $router->add ('users/store', ['controller'=>\App\Controllers\UsersController::class, 'action'=>'register', 'method'=>'POST']);
