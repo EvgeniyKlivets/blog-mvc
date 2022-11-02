9311ef869c6cdb1d3abc5bdb303de800fc634f81
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-11-02 21:58:44 +0200

    Update PostsController.php

--- App/Controllers/Admin/PostsController.php
+++ App/Controllers/Admin/PostsController.php
@@ -2,7 +2,44 @@
 
 namespace App\Controllers\Admin;
 
+use App\Models\Post;
+use Core\View;
+
 class PostsController extends BaseController
 {
+    public function index()
+    {
+        $posts = Post::all();
+        View::render('admin/posts/index', ['posts' => $posts]);
+    }
 
+    public function create()
+    {
+        View::render('admin/posts/create');
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show(int $id)
+    {
+
+    }
+
+    public function edit(int $id)
+    {
+
+    }
+
+    public function update(int $id)
+    {
+
+    }
+
+    public function destroy(int $id)
+    {
+
+    }
 }
